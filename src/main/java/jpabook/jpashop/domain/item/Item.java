9aa60b562022a64cc2f4437052a0b3eeadf4bc7a
jerry-1211 <kcjerim1234567@gmail.com>
2025-04-08 00:44:38 +0900

    상품 엔티티 개발 (비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
 import lombok.Getter;
@@ -26,4 +27,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    // 비즈니스 로직 추가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
