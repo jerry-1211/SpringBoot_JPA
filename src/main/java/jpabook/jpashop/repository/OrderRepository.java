6f5fbd70fa7c1a6bf0a80f5e076523babe152721
jerry-1211 <kcjerim1234567@gmail.com>
2025-04-09 16:42:48 +0900

    주문 서비스

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 public class OrderRepository {
     private final EntityManager em;
 
-    private void save(Order order){
+    public void save(Order order){
         em.persist(order);
     }
 
