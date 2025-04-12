c91b952cc44a35bcfb33a3ca917ae7fe6c628110
jerry-1211 <kcjerim1234567@gmail.com>
2025-04-12 17:35:18 +0900

    간단 주문 조회 V1 - 양방향 매핑 시 @JsonIgnore 필요 - V1은 엔티티가 노출되어 있음 (안티패턴) - 연관 관계의 모든 데이터를 가져오는게 아니라 필요한 정보만 가져오려고 하는 중

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,17 +1,29 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = " + data);
-
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	// hibernate5Modul는 지연 로딩 무시 (LAZY)
+	Hibernate5JakartaModule hibernate5Module() {
+		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+
+
+		/**
+		 * 이렇게 옵션을 줘서 Lazy 로딩되는 부분을 바로 로딩 시킬 수 있다.
+		 * 하지만 이렇게 하이버네이트 모듈 자체를 Lazy 로딩 때문에 등록하는거는 좋지 않음*/
+		// hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+
+
+		return hibernate5JakartaModule;
 	}
 }
